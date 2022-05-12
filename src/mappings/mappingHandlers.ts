17eddc1892789c02d0a14583b248ed2f0bb69507
chenhua790618 <aasoft@163.com>
2022-05-12 09:36:08 +0800

    crg

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -4,7 +4,7 @@ import { BigNumber } from "ethers";
 
 // Setup types from ABI
 type GovernEventArgs = [string, BigNumber, BigNumber, BigNumber, BigNumber, BigNumber, string, BigNumber] & {creator: string; number: BigNumber; governType: BigNumber; startDate: BigNumber; endDate: BigNumber; uintValue: BigNumber; strValue: string; totalVoter:BigNumber;};
-type VoteEventArgs = [string, BigNumber, BigNumber, BigNumber, BigNumber, BigNumber, boolean] & {votor: string; number: BigNumber; governType: BigNumber; totalVoter: BigNumber; approveVoter: BigNumber; opposeVoter:BigNumber; success:boolean;};
+type VoteEventArgs = [string, BigNumber, BigNumber, BigNumber, BigNumber] & {votor: string; number: BigNumber; governType: BigNumber; state: BigNumber; voters: BigNumber;};
 
 export async function handleGovernEvmEvent(event: FrontierEvmEvent<GovernEventArgs>): Promise<void> {
     const startGovern = new StartGovern(event.transactionHash);
@@ -28,10 +28,8 @@ export async function handleVoteEvmEvent(event: FrontierEvmEvent<VoteEventArgs>)
     voteByNumber.votor = event.args.votor;
     voteByNumber.number = event.args.number.toBigInt();
     voteByNumber.governType = event.args.governType.toBigInt();
-    voteByNumber.totalVoter = event.args.totalVoter.toBigInt();
-    voteByNumber.approveVoter = event.args.approveVoter.toBigInt();
-    voteByNumber.opposeVoter = event.args.opposeVoter.toBigInt();
-    voteByNumber.success = event.args.success;
+    voteByNumber.state = event.args.state.toBigInt();
+    voteByNumber.voters = event.args.voters.toBigInt();
     voteByNumber.contractAddress = event.address;
 
     await voteByNumber.save();
